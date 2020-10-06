55fc0742b8024e7e1621133607a5e434904dc095
itsgergoo <71039056+itsgergoo@users.noreply.github.com>
2020-10-06 11:19:46 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace method
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Main program és b-je");
+            int a = 10;
+            int b = 20;
+            Console.WriteLine(a + b);
+            calc(ref a, b);
+            Console.WriteLine("függvény eljárás után");
+            b = calc(ref a, b);
+            Console.WriteLine(a + b);
+            Console.ReadLine();
+            
+        }
+        static int calc(ref int a, int b)
+        {
+            Console.WriteLine("Eljárás és b-je");
+            Console.WriteLine(a + b);
+            a = 100;
+            Console.WriteLine("Eljárájban a változik és kiíratom");
+            Console.WriteLine(a + b);
+            return b + 50;
+
+        }
+    }
+}
